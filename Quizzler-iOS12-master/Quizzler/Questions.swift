e9cb8fc635c5c641e20a0ad8f7f3717bd27078c2
Nguyen Hieu Minh <shenlong2361996@gmail.com>
2018-12-17 02:11:37 +0200

    Initial commit

---
+++ Quizzler-iOS12-master/Quizzler/Questions.swift
@@ -0,0 +1,27 @@
+//
+//  Questions.swift
+//  Quizzler
+//
+//  Created by Minh on 12/16/18.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Questions{
+    
+    let question: String
+    let answer: Bool
+    
+    init(questionText: String, correctAnswer: Bool) {
+        question = questionText
+        answer = correctAnswer
+        
+    }
+    
+}
+
+class cc{
+    let a = Questions(questionText: "are u gay?", correctAnswer: false)
+    
+}
